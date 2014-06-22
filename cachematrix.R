06d3f0b09977bd88bc27b30d3290142a28085e6d
rcoutopt <rcouto@creativethinkers.eu>
2014-06-22 09:05:20 +0100

    File finnished

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This funcion creates a list of  functions to set and get the value of a matrix, and set and get the result of the function solve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setsolve <- function(solve) m <<- solve
+      getsolve <- function() m
+      list(set = set, get = get,
+           setsolve = setsolve,
+           getsolve = getsolve)      
 }
 
 
-## Write a short comment describing this function
+## this function calculate the result of the function solve of the matrix returned by the function makeCacheMatrix
+## how ever before make the calculation check is the calculation have already been made and stored the "special matrix" 
+## returned by the function makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getsolve()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setsolve(m)
+      m
 }
